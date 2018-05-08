bf4966bd34c79d9a73f8948e5cc1887ce4596b7b
Jack Ogina <jackogina60@gmail.com>
2018-05-09 01:19:23 +0300

    added a command for addingnew user accounts&terminating accounts

--- pass_cli/pass_cli.py
+++ pass_cli/pass_cli.py
@@ -30,6 +30,39 @@ def cli(username):
     '''
     click.echo(click.style(pass_cli.config.logo,fg="blue"))
 
+@cli.command(help='Create new credential to database')
+@click.option('--name', prompt='Username',help="Username for password manager account")
+@click.password_option(help="Master password to decrypt / encrypt password files")
+def new_account(name,password):
+    '''
+    Handles user log in sessions
+    '''
+    if SessionCheck(name).check_if_user_exists():
+        return click.echo(click.style("Account name: {} already exists".format(name), bg='red', fg='white'))
+    if name.startswith(crypto_extention):
+        return click.echo(click.style("Account name cannot start with a {}".format(crypto_extention), bg='red', fg='white'))
+    pass_cli.manage_users.User_Account(name,password).create_account()
+    return click.echo(click.style("Account name successfully created for {}".format(name), bg='green', fg='white'))
+
+@cli.command(help='Delete Account From Pass Cli This will erase all your credentials virtually for good')
+@click.option('--name', prompt='Username',help="Username for password manager account")
+@click.password_option(help="Master password to decrypt / encrypt password files")
+def terminate_account(name,password):
+    '''
+    Handles Deleting of a User Account
+    '''
+    click.echo(click.style("{}".format(pass_cli.config.terminate_warning),fg='white'))
+    confirm_state=input("Enter (yes) if you wish to continue or any key to halt : ")
+    if confirm_state!="yes": return click.echo(click.style("Process has been halted", bg='yellow', fg='white'))
+    if  not SessionCheck(name).check_if_user_exists():
+        return click.echo(click.style("Account name: {} does not  already exists".format(name), bg='red', fg='white'))
+    if pass_cli.manage_users.User_Account(name,password).terminate_account():
+        return click.echo(click.style("Account  name {} has been successfully terminated".format(name), bg='green', fg='white'))
+    elif ValueError==type(pass_cli.manage_users.User_Account(name,password).terminate_account()):
+        return click.echo(click.style("Wrong password given for account name: {}".format(name), bg='red', fg='white'))
+    else:
+        return click.echo(click.style("Unable to terminate account name: {}".format(name), bg='red', fg='white'))
+
 
 if __name__=="__main__":
     cli()
