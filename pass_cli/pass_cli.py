f42a81193ea204e1127d184304128462967241a9
Jack Ogina <jackogina60@gmail.com>
2018-05-09 01:23:29 +0300

    added new features for searching and adding new creds to HP

--- pass_cli/pass_cli.py
+++ pass_cli/pass_cli.py
@@ -105,6 +105,69 @@ def logout(name,password):
     else:
         return click.echo(click.style("Unable to log out account name: {}".format(name), bg='red', fg='white'))
 
+@cli.command(help="Log out of an already existing account")
+@click.option('--name', prompt='Username',help="Username for password manager account")
+@click.option('-r', '--random', is_flag=True, help="Randomly generate password")
+def add_cred(name,random):
+    session_data=SessionCheck(name).get_user_session()
+    if session_data==True:
+        click.echo(click.style("Warning: duplicate names may errors during a search Enter unique credential names",fg="red"))
+        cred_name=click.prompt("Unique credential name", type=str)
+        cred_login=click.prompt("Enter credential login/username:", type=str)
+        if random==True:
+            rand_length = click.prompt("Enter length of random password:", type=int)
+            rand_str=CSRNG(rand_length).hex_osrandom()
+            User_Creds(name).add([cred_name,cred_login,rand_str])
+            return click.echo(click.style("Credentials successfully added", bg='green', fg='white'))
+        else:
+            web_pass=getpass("Enter credential password:")
+            web_pass2=getpass("Confirm credential password:")
+            if web_pass !=web_pass2:
+                return click.echo(click.style("Passwords do not match try again", bg='red', fg='white'))
+            User_Creds(name).add([cred_name,cred_login,web_pass])
+            return click.echo(click.style("Credentials successfully added", bg='green', fg='white'))
+
+    if type(session_data)==ValueError:
+        return click.echo(click.style("Account  name {} does not  exist".format(name), bg='red', fg='white'))
+    elif session_data==False:
+        return click.echo(click.style("Account name: {} is logged out".format(name), bg='red', fg='white'))
+
+@cli.command(help="Search for user data and copy password to clip board")
+# @click.option('-s', '--show', is_flag=False, help="show passwords in clear text")
+@click.option('--username', prompt='Username',help="Username for password manager account")
+def search(username):
+    session_data=SessionCheck(username).get_user_session()
+    if session_data==True:
+        credentials=User_Creds(username).get_creds()
+        if len(credentials)>=1:
+            table = Table(
+                pass_cli.config.fieldnames,'rst',
+                colors=pass_cli.config.colors,
+                hidden=[pass_cli.config.fieldnames[2]]
+            )
+            click.echo(table.render(credentials))
+            if click.confirm('Do you want to get password of any of the displayed sites?'):
+                cred_name=click.prompt("Unique credential name", type=str)
+                search_data=User_Creds(username).get_creds()
+                search_pass=''
+                for row in search_data:
+                    if row[pass_cli.config.fieldnames[0]]==cred_name:
+                        search_pass=row[pass_cli.config.fieldnames[2]]
+                        break
+                if search_pass:
+                    pyperclip.copy(search_pass)
+                    return click.echo(click.style("Found password for  {}..password copied to clipboard".format(cred_name), bg='green', fg='white'))
+                else:
+                    return click.echo(click.style("Did not Find password for  {}".format(cred_name), bg='red', fg='white'))
+        elif len(credentials)<1:
+            return click.echo(click.style("Empty data for account name: {}".format(username), bg='red', fg='white'))
+        else:
+            return click.echo(click.style("Unable to display credentials", bg='red', fg='white'))
+    if type(session_data)==ValueError:
+        return click.echo(click.style("Account  name {} does not  exist".format(username), bg='red', fg='white'))
+    elif session_data==False:
+        return click.echo(click.style("Account name: {} is logged out".format(username), bg='red', fg='white'))
+
 
 if __name__=="__main__":
     cli()
